a5a6e6e1f2ee2fcc839a2a6dcd2946386a72dacc
maftuhlutfi <playmakernyomlo@gmail.com>
2020-06-24 11:09:09 +0700

    Updates

---
+++ precache-manifest.a11f2351e0d0ece3e5a746c719cc0273.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd78b1d5ce6b64afcb62c7f3c7184ba",
+    "url": "/react-redux-simple-shop-cart-app/index.html"
+  },
+  {
+    "revision": "eee01539a8c194bc8629",
+    "url": "/react-redux-simple-shop-cart-app/static/js/2.95fc8b85.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-redux-simple-shop-cart-app/static/js/2.95fc8b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef6f0fd1d860618fbabb",
+    "url": "/react-redux-simple-shop-cart-app/static/js/main.2523eee6.chunk.js"
+  },
+  {
+    "revision": "4ca264856c5f3b34ceec",
+    "url": "/react-redux-simple-shop-cart-app/static/js/runtime-main.2bc5d963.js"
+  }
+]);
